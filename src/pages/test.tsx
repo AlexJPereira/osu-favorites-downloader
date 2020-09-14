1da25e2554a6356d0cf5ab5d85124bbd340b3f96
Alex Junior Pereira <alexjp17@gmail.com>
2020-09-14 19:12:41 -0300

    add timer function for test

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,9 +1,22 @@
 import React, {Component} from 'react'
 
+export interface ITestProps {}
+
 export default class Test extends Component{
+    state = {
+        timerCount: 0
+    }
+
+    constructor(props: ITestProps){
+        super(props)
+        setInterval(()=>{
+            this.setState({timerCount: this.state.timerCount + 1})
+        }, 1000)
+    }
+
     render(){
         return (
-            <h1>Hello</h1>
+            <h1>Counting... {this.state.timerCount}</h1>
         )
     }
 }
