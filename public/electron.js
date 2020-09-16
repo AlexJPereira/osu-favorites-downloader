a81aee5d17d3f27929f123655478e103df5ed79c
Alex Junior Pereira <alexjp17@gmail.com>
2020-09-16 20:20:33 -0300

    add electron

---
+++ public/electron.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev')
+ 
+const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 600,
+    webPreferences: {
+      worldSafeExecuteJavaScript: true,
+      nodeIntegration: true
+    }
+  })
+
+  win.setMenu(null)
+  win.loadURL(startURL)
+
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
