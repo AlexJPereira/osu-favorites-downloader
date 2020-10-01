c97fd440febbae6e05abf2b9075bc08c3819af45
Alex Junior Pereira <alexjp17@gmail.com>
2020-10-01 20:41:55 -0300

    change the function get header to getHeader

--- src/electron/src/api/osuApi.ts
+++ src/electron/src/api/osuApi.ts
@@ -1,25 +1,54 @@
 import axios from 'axios'
 import axiosCookieJarSupport from 'axios-cookiejar-support'
 import tough from 'tough-cookie'
+import dotenv from 'dotenv'
+
 import delay from '../utils/delay'
+import { stringify } from 'querystring'
 
-axiosCookieJarSupport(axios);
+dotenv.config({path: "../../.env"})
+axiosCookieJarSupport(axios)
+axios.defaults.withCredentials = true
 
+const envUsername = process.env.OSU_USERNAME || ""
+const envPassword = process.env.OSU_PASSWORD || ""
 
 export default class OsuApi{
-    private cookieJar = new tough.CookieJar();
+    private cookieJar = new tough.CookieJar()
     private globalDelay = 1000
+    private xsrfToken = ""
 
     async tests(){
         await this.getCookies()
-        console.log(this.cookieJar)
+        const resp = await this.loginOsuUser(envUsername, envPassword)
+        console.log(resp)
     }
     
     async getCookies(){
-        await axios.get("https://osu.ppy.sh/home",{
-            jar: this.cookieJar,
-            withCredentials: false
-        })
+        const starterPage = await axios.get("https://osu.ppy.sh/home", { jar: this.cookieJar })
+        const xsrfTokenName = starterPage.config.xsrfCookieName || ""
+        this.xsrfToken = this.cookieJar.getCookiesSync("https://osu.ppy.sh/home").find(cookie => cookie.key === xsrfTokenName)?.value || ""
         await delay(this.globalDelay)
     }
+
+    async loginOsuUser(username: string, password: string){
+        return await axios.post("https://osu.ppy.sh/session",stringify({
+            _token: this.xsrfToken,
+            username,
+            password
+        }), {
+            headers: this.getHeader(),
+        })
+    }
+
+    getHeader(){
+        return {
+            "X-CSRF-Token": this.xsrfToken,
+            Cookie: this.cookieJar.getCookieStringSync("https://osu.ppy.sh/home"),
+            "Referer": "https://osu.ppy.sh/home",
+        }
+    }
 }
+
+const test = new OsuApi()
+test.tests()
