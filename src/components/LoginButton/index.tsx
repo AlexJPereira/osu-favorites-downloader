6835ce2a81e94f4fc2836f7ccb91c2a633433bd0
Alex Junior Pereira <alexjp17@gmail.com>
2020-10-02 21:41:39 -0300

    add login button

---
+++ src/components/LoginButton/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './style.css'
+
+export default function(){
+    return (
+        <button className="login-button">Sign in</button>
+    )
+}
--- src/components/LoginCard/index.tsx
+++ src/components/LoginCard/index.tsx
@@ -3,15 +3,23 @@ import React from 'react'
 import './style.css'
 import LoginInput from '../LoginInput'
 import ForgotPassword from '../ForgotPassword'
+import LoginButton from '../LoginButton'
 
 export default class LoginCard extends React.Component{
     render(){
         return (
             <div className="login-page">
                 <div className="login-card">
-                    <LoginInput placeholder="Username"/>
-                    <LoginInput placeholder="Password" password/>
-                    <ForgotPassword/>
+                    <div className="login-card-top">
+                        <LoginInput placeholder="Username"/>
+                        <LoginInput placeholder="Password" password/>
+                        <div className="forgot-password-container">
+                            <ForgotPassword/>
+                        </div>
+                    </div>
+                    <div className="login-card-bottom">
+                        <LoginButton/>
+                    </div>
                 </div>
             </div>
         )
