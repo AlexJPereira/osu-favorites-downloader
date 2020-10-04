c53562515919fcae4b8b51afc0e03770bc4c5333
Alex Junior Pereira <alexjp17@gmail.com>
2020-10-04 02:49:53 -0300

    add create favorite list

--- src/electron/src/api/osuApi.ts
+++ src/electron/src/api/osuApi.ts
@@ -2,13 +2,13 @@ import axios from 'axios'
 import axiosCookieJarSupport from 'axios-cookiejar-support'
 import tough from 'tough-cookie'
 import dotenv from 'dotenv'
-import { stringify } from 'querystring'
 
 import OsuUser from './osuUser'
 
 import OsuLogin from './osuLogin'
 import OsuDownload from './osuDownload'
 import OsuFavoriteList from './osuFavoriteList'
+import osuFavoriteList from './osuFavoriteList'
 
 dotenv.config({path: "../../.env"})
 axiosCookieJarSupport(axios)
@@ -18,15 +18,19 @@ const envPassword = process.env.OSU_PASSWORD || ""
 export default class OsuApi{
     protected cookieJar = new tough.CookieJar(undefined, { rejectPublicSuffixes: false })
     protected globalDelay = 1000
+    protected maxFavoriteCallCount = 50
     protected xsrfToken = ""
     protected xsrfTokenName = ""
+
+    public osuUser: OsuUser | undefined = undefined
 
     async tests(){
         // await this.getCookies()
         // const page = await this.loginOsuUser(envUsername, envPassword)
         // const wykke = new OsuUser(page)
         // console.log(wykke.userPic)
-        console.log(await this.getFavoriteCount(9866460))
+        const btmcount = await this.getFavoriteCount(9866460)
+        console.log((await this.getUserFavoriteBeatmapsIds(9866460, 0, btmcount)).length, btmcount)
     }
     
     getCookies = OsuLogin.getCookies.bind(this)
@@ -34,6 +38,8 @@ export default class OsuApi{
     loginOsuUser = OsuLogin.loginOsuUser.bind(this)
 
     getFavoriteCount = OsuFavoriteList.getFavoriteCount.bind(this)
+    getUserFavouriteBeatmaps = OsuFavoriteList.getUserFavouriteBeatmaps.bind(this)
+    getUserFavoriteBeatmapsIds = osuFavoriteList.getUserFavoriteBeatmapsIds.bind(this)
 
     downloadSingleBeatmap = OsuDownload.downloadSingleBeatmap.bind(this)
     saveBeatmap = OsuDownload.saveBeatmap
--- src/electron/src/api/osuFavoriteList.ts
+++ src/electron/src/api/osuFavoriteList.ts
@@ -2,14 +2,59 @@ import OsuApi from './osuApi'
 import delay from '../utils/delay'
 import axios from 'axios'
 
+interface IBeatmapFavoriteList{
+    artist: string
+    covers: {
+        cover: string
+        list: string
+    }
+    title: string
+    title_unicode: string
+    video: boolean
+    favourite_count: number
+    play_count: number
+    id: number
+    preview_url: string
+    creator: string
+}
+
 async function getFavoriteCount(this: OsuApi, userId: number){
     const userPage = await axios.get(`https://osu.ppy.sh/users/${userId}`, { jar: this.cookieJar })
     const favCountLocation = userPage.data.indexOf('"favourite_beatmapset_count":')
     const favTagLength = '"favourite_beatmapset_count":'.length
     const favCount = userPage.data.substring(favCountLocation + favTagLength, userPage.data.indexOf(",",favCountLocation))
-    return favCount
+    await delay(this.globalDelay)
+    return Number.parseInt(favCount)
+}
+
+async function getUserFavouriteBeatmaps(this: OsuApi, userId: number, offset: number, favoriteCount: number){
+    const favoritesPage = await axios.get(
+        `https://osu.ppy.sh/users/${userId}/beatmapsets/favourite?offset=${offset}&limit=${favoriteCount}`,
+        { jar: this.cookieJar }
+    )
+    const favoritesList = (favoritesPage.data) as IBeatmapFavoriteList[]
+    return favoritesList
+}
+
+async function getUserFavoriteBeatmapsIds(this: OsuApi, userId: number, offset: number, favoriteCount: number){
+    const favoriteIds: {id: number}[] = []
+    while(offset + this.maxFavoriteCallCount < favoriteCount){
+        const list = await this.getUserFavouriteBeatmaps(userId, offset, this.maxFavoriteCallCount)
+        list.forEach(beatmap => {
+            favoriteIds.push({id: beatmap.id})
+        })
+        offset += this.maxFavoriteCallCount
+        await delay(this.globalDelay)
+    }
+    const finalList = await this.getUserFavouriteBeatmaps(userId, offset, favoriteCount - offset)
+    finalList.forEach(beatmap => {
+        favoriteIds.push({id: beatmap.id})
+    })
+    return favoriteIds
 }
 
 export default {
-    getFavoriteCount
+    getFavoriteCount,
+    getUserFavouriteBeatmaps,
+    getUserFavoriteBeatmapsIds
 }
