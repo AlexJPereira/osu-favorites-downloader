3fd980e8f85a4eff13fd5e2d926033414335e3e5
Alex Junior Pereira <alexjp17@gmail.com>
2020-10-11 02:11:40 -0300

    add a function to get the first five favorite beatmaps and link them with the beatmap list component

--- src/components/BeatmapList/index.tsx
+++ src/components/BeatmapList/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
 
 import './style.css'
+import { IBeatmapFavoriteList } from '../../electron/src/api/osuFavoriteList'
+
+const { ipcRenderer } = window.require("electron");
 
 export interface IBeatmapListProps{
     userId: number
 }
 
 export default class BeatmapList extends React.Component<IBeatmapListProps>{
+    state = {
+        favoriteList: [] as IBeatmapFavoriteList[]
+    }
+
+    constructor(props: IBeatmapListProps){
+        super(props)
+        ipcRenderer.send("getFavoriteList", props.userId)
+        ipcRenderer.on("getFavoriteListReply", (event, beatmapList: IBeatmapFavoriteList[])=>{
+            this.setState({ favoriteList: beatmapList })
+        })
+    }
     render(){
         return(
             <div className="beatmaplist-container">
                 <ul className="beatmaplist-list">
-                    {()=>{
-
-                    }}
+                    {this.state.favoriteList.map(()=>{
+                        
+                    })}
                 </ul>
             </div>
         )
