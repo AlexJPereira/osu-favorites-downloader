d7c9cccadc81752b3a7671216aa7c1978d84af73
Alex Junior Pereira <alexjp17@gmail.com>
2020-10-10 23:06:54 -0300

    add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import MainPage from './pages/Login'
+import Router from './routes'
 
-ReactDOM.render(
-    <MainPage/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Router/>, document.getElementById('root'));
 
 
