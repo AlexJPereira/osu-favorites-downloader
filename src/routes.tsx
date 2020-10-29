5e16266e17d8d8c1f1d5b30905da6151e005861c
Alex Junior Pereira <alexjp17@gmail.com>
2020-10-29 15:17:43 -0300

    add icon and use hash router instead of browser router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 
 import Login from './pages/Login'
 import Download from './pages/Download'
 
 export default function Routes(){
     return(
-        <BrowserRouter>
+        <HashRouter>
             <Switch>
                 <Route path="/" exact component={Login}/>
                 <Route path="/download" component={Download}/>
             </Switch>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
